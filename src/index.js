7a3659ba0ffe4c355ef2dac4cf7a21e59c7105f6
Gabriel Bordenabe <gabrielbordenabe@gmail.com>
2024-06-01 10:14:33 -0300

    chore: Update route for greeting endpoint

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-app.post('*', async (req, res) => {
-  res.send(req.body);
-});
-
-app.get('*', async (req, res) => {
-  res.send('Hello World!');
+app.get('/greet', async (req, res) => {
+  res.send('Hello World');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
